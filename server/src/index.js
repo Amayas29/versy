75ea10da7169ced759f6254b8f5c53049d3ff833
Dayane <makhlouf.rayane2001@gmail.com>
2022-04-29 05:33:00 +0200

    nodemon and dotenv added succesfuly

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const app = require("./app.js");
 const port = 4000;
+require('dotenv').config( {path: './config.env'} );
 
-app.default.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.default.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
