3e26b1203cc8b81a15b8cbab0ccd486d9106c124
Amayas29 <amayas29sd@gmail.com>
2022-05-06 23:54:00 +0200

    fix hash

--- server/src/utils/hash.js
+++ server/src/utils/hash.js
@@ -4,8 +4,8 @@ function hash(password) {
   return bcrypt.hash(password, 10);
 }
 
-function compare(password, hash) {
-  return bcrypt.compare(password, hash);
+function compare(password, hashpassword) {
+  return bcrypt.compare(password, hashpassword);
 }
 
 module.exports = { hash, compare };
